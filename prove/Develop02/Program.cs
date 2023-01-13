aa88dc98c5706639896b66fd8a630235d78d55ed
robgol <rmvcorreia@gmail.com>
2023-01-13 02:43:56 -0100

    Team Activity

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        Journal myJournal = new Journal();
+
+        myJournal.Write();
+        myJournal.Write();
+
+
+        myJournal.Display();
+        myJournal.SaveToFile();
+
         Console.WriteLine("Hello Develop02 World!");
     }
+
+
+    public void DisplayMenu()
+    {
+        string[] menuList = {"1. Write", "2. Display", "3. Load", "4. Save", "5. Quit"};
+        foreach (string menu in menuList)
+        {
+            
+        }
+    }
+
 }
