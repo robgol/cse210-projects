046364af68902397b7d8d7265543389ff72a453a
robgol <rmvcorreia@gmail.com>
2023-01-16 15:56:52 -0100

    Preparation week 3 done!

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,48 @@
+class Fraction
+{
+    private int _top;
+    private int _bottom = 1;
+
+    public Fraction(){}
+
+    public Fraction(int wholeNumber)
+    {
+       SetTop(wholeNumber);
+    }
+
+    public Fraction (int top, int bottom)
+    {
+        SetTop(top);
+        SetBottom(bottom);
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return _top + "/" + _bottom;
+    }
+
+    public double GertDecimalValue()
+    {
+        return _top / Convert.ToDouble(_bottom);
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fra1 = new Fraction(1);
+        Fraction fra2 = new Fraction(5);
+        Fraction fra3 = new Fraction(3, 4);
+        Fraction fra4 = new Fraction(1, 3);
+
+        List<Fraction> fractions = new List<Fraction>();
+        fractions.Add(fra1);
+        fractions.Add(fra2);
+        fractions.Add(fra3);
+        fractions.Add(fra4);
+
+        foreach (Fraction item in fractions)
+        {
+            Console.WriteLine(item.GetFractionString());
+            Console.WriteLine(item.GertDecimalValue());
+        }
+
     }
 }
